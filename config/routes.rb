14d61dc23281da0da4ca5fb5e636e6c3f6e9808e
Kisei Takinishi <Kisei@Kisei-no-MacBook-Air.local>
2018-05-16 16:22:54 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
 
+  root 'home#top'
 
-  get 'homes/top'
+  get  '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "homes#top"
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  post '/reviews', to: 'reviews#create'
+
+  # get '/auth/:provider/callback',    to: 'users#create',       as: :auth_callback
+  # get '/auth/failure',               to: 'users#auth_failure', as: :auth_failure
+
+  resources :posts
+  resources :messages
+  resources :users
 end
